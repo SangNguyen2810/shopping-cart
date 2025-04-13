d376a6a4b10ff8a0dce65f00bc66c847c5c9f611
Sang Nguyen <nguyenquangsang2810@gmail.com>
2025-04-13 10:22:53 +0700

    [Feat] readme update

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,11 +1,19 @@
 // tailwind.config.js
+/** @type {import('tailwindcss').Config} */
 export default {
   content: [
     "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx,css}", // 👈 include `.css` files!
+    "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: {
+          dark: '#1a1a1a',
+          darker: '#000000'
+        }
+      }
+    },
   },
   plugins: [],
 };
